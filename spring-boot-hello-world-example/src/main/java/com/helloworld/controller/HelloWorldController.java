1af64f3031b5b7a914a39ddb58b9ce523160ae5d
Taylor Thurman <taylor.thurman@protonmail.com>
2023-03-08 20:27:48 -0600

    Add import

--- spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
+++ spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
@@ -1,6 +1,6 @@
 package com.helloworld.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
