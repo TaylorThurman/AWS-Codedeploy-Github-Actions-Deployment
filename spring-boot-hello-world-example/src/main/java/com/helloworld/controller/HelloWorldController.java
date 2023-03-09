3b8f2d186a2ebaeeb0c1db86fe99d3466db2f4b8
Taylor Thurman <taylor.thurman@protonmail.com>
2023-03-08 20:25:05 -0600

    Add another test endpoint

--- spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
+++ spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
@@ -1,12 +1,17 @@
 package com.helloworld.controller;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
-public class HelloWorldController 
-{
-@RequestMapping("/")
-public String hello() 
-{
-return "<h1> Congratulations. You have successfully deployed the sample Spring Boot Application. </h1>";
-}
+public class HelloWorldController {
+    @RequestMapping("/")
+    public String hello() {
+        return "<h1> Congratulations. You have successfully deployed the sample Spring Boot Application. </h1>";
+    }
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello World";
+    }
 }
