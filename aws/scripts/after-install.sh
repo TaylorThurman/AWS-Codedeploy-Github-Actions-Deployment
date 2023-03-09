dfb72055fd1b2b24f39fbaa58bf78559e3527e72
Taylor Thurman <taylor.thurman@protonmail.com>
2023-03-09 10:12:07 -0600

    Change s3 bucket location

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -4,7 +4,7 @@ set -xe
 
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://codedeploystack-webappdeploymentbucket-l47k11fbtdg9/SpringBootHelloWorldExampleApplication.war /usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
+aws s3 cp s3://codedeploystack-webappdeploymentbucket-1afsdpvnr01th/SpringBootHelloWorldExampleApplication.war /usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
 
 
 # Ensure the ownership permissions are correct.
