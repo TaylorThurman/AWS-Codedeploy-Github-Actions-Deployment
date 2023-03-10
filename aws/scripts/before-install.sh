2f648c8f02a97fcfd9ea31a86b8d528b0587736c
Taylor Thurman <taylor.thurman@protonmail.com>
2023-03-09 19:13:11 -0600

    Add back in codedeployresources directory

--- aws/scripts/before-install.sh
+++ aws/scripts/before-install.sh
@@ -3,6 +3,12 @@ sudo su -
 set -xe
 
 # Delete the old  directory as needed.
+if [ -d /usr/local/codedeployresources ]; then
+    rm -rf /usr/local/codedeployresources/
+fi
+
+mkdir -vp /usr/local/codedeployresources
+
 if [ -d /app ]; then
     rm -rf /app
 fi
