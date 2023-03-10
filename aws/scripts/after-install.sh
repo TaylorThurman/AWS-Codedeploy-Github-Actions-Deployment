9d89bf7c7c08fbdda1f0707f47be08c025116bf5
Taylor Thurman <taylor.thurman@protonmail.com>
2023-03-10 11:02:02 -0600

    New stack. Add back in codedeploy install

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -4,4 +4,4 @@ set -xe
 
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://codedeploystack-webappdeploymentbucket-j8m4upmtglrj/SpringBootHelloWorldExampleApplication.jar /home/ec2/app/SpringBootHelloWorldExampleApplication.jar
+aws s3 cp s3://codedeploystack-webappdeploymentbucket-k846gl0udjfl/SpringBootHelloWorldExampleApplication.jar /home/ec2/app/SpringBootHelloWorldExampleApplication.jar
