012a6859718289ae34df00995f94b5624f13de56
Taylor Thurman <taylor.thurman@protonmail.com>
2023-03-10 12:24:55 -0600

    Change script

--- aws/scripts/application-start.sh
+++ aws/scripts/application-start.sh
@@ -3,5 +3,4 @@ sudo su -
 set -xe
 echo "hi"
 cd app
-touch nohup.out
-nohup java -jar SpringBootHelloWorldExampleApplication.jar > nohup.out & tail -n 100 nohup.out
+nohup java -jar SpringBootHelloWorldExampleApplication.jar
